4e31787681800bdf9fd0da8e2ae69ba4e86f0bbe
Mike Baker <bakermg@me.com>
2017-03-28 17:36:18 -0400

    First commit on arepasports theme built with underscores components

---
+++ components/post/content-footer.php
@@ -0,0 +1,3 @@
+	<footer class="entry-footer">
+		<?php arepasports_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
---
+++ components/post/content-meta.php
@@ -0,0 +1,3 @@
+		<div class="entry-meta">
+			<?php arepasports_posted_on(); ?>
+		</div><!-- .entry-meta -->
